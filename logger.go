f421052d70817210660ade4a14bd03f7cb8295c6
krishpranav <krisna.pranav@gmail.com>
2021-12-26 17:38:01 +0530

    logger: logger handler

--- logger.go
+++ logger.go
@@ -7,14 +7,22 @@ import (
 )
 
 func Logger() Handler {
-	return func(res http.ResponseWriter, req *http.Requet, c Context, log *log.Logger) {
+	return func(res http.ResponseWriter, req *http.Request, c Context, log *log.Logger) {
 		start := time.Now()
 
 		addr := req.Header.Get("X-Real-IP")
 		if addr == "" {
 			addr = req.Header.Get("X-Forwarded-For")
+			if addr == "" {
+				addr = req.RemoteAddr
+			}
 		}
-	}
 
-	log.Printf("Started %s %s for %s", req.Method, req.URL.Path, addr)
+		log.Printf("Started %s %s for %s", req.Method, req.URL.Path, addr)
+
+		rw := res.(ResponseWriter)
+		c.Next()
+
+		log.Printf("Completed %v %s in %v\n", rw.Status(), http.StatusText(rw.Status()), time.Since(start))
+	}
 }
