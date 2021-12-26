48d8da1d122d5043b5d29dbf13762c58dafa4fda
krishpranav <krisna.pranav@gmail.com>
2021-12-26 12:05:34 +0530

    static: serve the static files

--- static/static.go
+++ static/static.go
@@ -1,1 +1,49 @@
 package static
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"strings"
+)
+
+type StaticOptions struct {
+	Prefix      string
+	SkipLogging bool
+	IndexFile   string
+	Expires     func() string
+	Fallback    string
+	Exclude     string
+}
+
+func prepareStaticOptions(options []StaticOptions) {
+	var opt StaticOptions
+	if len(options) > 0 {
+		opt = options[0]
+	}
+
+	if len(opt.IndexFile) == 0 {
+		opt.IndexFile = "index.html"
+	}
+
+	if opt.Prefix != "" {
+		if opt.Prefix[0] != '/' {
+			opt.Prefix = "/" + opt.Prefix
+		}
+		opt.Prefix = strings.TrimRight(opt.Prefix, "/")
+	}
+}
+
+/* serves the static */
+func Static(directory string, staticOpt ...StaticOptions) Handler {
+	if !filepath.IsAbs(directory) {
+		directory = filepath.Join(Root, directory)
+	}
+	dir := http.Dir(directory)
+	opt := prepareStaticOptions(staticOpt)
+	
+	return func(res http.ResponseWriter, req *http.Request, log *log.Logger) {
+
+	}
+
+}
