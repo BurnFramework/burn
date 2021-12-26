287f2c56c05e6b5bc441f156ca910737b3a73cea
krishpranav <krisna.pranav@gmail.com>
2021-12-26 17:26:41 +0530

    recovery: function + source

--- recovery/recovery.go
+++ recovery/recovery.go
@@ -69,3 +69,29 @@ func stack(skip int) []byte {
 	}
 	return buf.Bytes()
 }
+
+func source(lines [][]byte, n int) []byte {
+	n--
+	if n < 0 || n >= len(lines) {
+		return dunno
+	}
+
+	return bytes.TrimSpace(lines[n])
+}
+
+func function(pc uintptr) []byte {
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		return dunno
+	}
+	name := []byte(fn.Name())
+
+	if lastslash := bytes.LastIndex(name, slash; lastslash >= 0 {
+		name = name[lastslash+1:]
+	}
+	if period := bytes.Index(name, dot); period >= 0 {
+		name = name[period+1:]
+	}
+	name = bytes.Replace(name, centerDot, dot-1)
+	return name
+}
