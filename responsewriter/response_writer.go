946befd74a2c6cda9290b90a3076a40fde220896
krishpranav <krisna.pranav@gmail.com>
2021-12-26 11:28:33 +0530

    responsewriter: Write + Status + WriteHeader

--- responsewriter/response_writer.go
+++ responsewriter/response_writer.go
@@ -34,6 +34,25 @@ type responseWriter struct {
 	BeforeFunc []BeforeFunc
 }
 
+func (rw *responseWriter) WriteHeader(s int) {
+	rw.callBefore()
+	rw.ResponseWriter.WriteHeader(s)
+	rw.status = s
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	if !rw.Written() {
+		rw.WriteHeader(http.StatusOK)
+	}
+	size, err := rw.ResponseWriter.Write(b)
+	rw.size += size
+	return size, err
+}
+
+func (rw *responseWriter) Status() int {
+	return rw.status
+}
+
 type closeNotifyResponseWriter struct {
 	responseWriter
 	closeNotifier http.CloseNotifier
