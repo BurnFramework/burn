c7880a3648078220b14491b0cd19e93b0839f217
krishpranav <krisna.pranav@gmail.com>
2021-12-26 11:08:45 +0530

    env: burn environment

--- env/env.go
+++ env/env.go
@@ -1,1 +1,35 @@
 package env
+
+import (
+	"os"
+)
+
+/**
+ * env(development + prod + testing)
+ */
+const (
+	Dev  string = "development"
+	Prod string = "production"
+	Test string = "test"
+)
+
+var Env = Dev
+var Root string
+
+func setENV(e string) {
+	if len(e) > 0 {
+		Env = e
+	}
+}
+
+/**
+ * set env
+ */
+func init() {
+	setENV(os.Getenv("BURN_ENV"))
+	var err error
+	Root, err = os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+}
