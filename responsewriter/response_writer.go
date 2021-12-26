03a3737fdd7dd3d3d21215720fa6c9f1a8d07bdc
krishpranav <krisna.pranav@gmail.com>
2021-12-26 11:27:35 +0530

    responsewriter: responses
    new response writer + responsewriter struct[status, size, beforefunc] +
    close notify response writer

--- responsewriter/response_writer.go
+++ responsewriter/response_writer.go
@@ -1,22 +1,40 @@
 package responsewriter
 
 import (
-  "net"
-  "net/http"
+	"net/http"
 )
 
 type ResponseWriter interface {
-    http.ResponseWriter
-    http.Flusher
-    http.Hijacker
-    
-    Status() int
-    Size() int
-    
-    Writer() bool
-    
-    Before(BeforeFunc)
+	http.ResponseWriter
+	http.Flusher
+	http.Hijacker
+
+	Status() int
+	Size() int
+
+	Writer() bool
+
+	Before(BeforeFunc)
 }
 
 type BeforeFunc func(ResponseWriter)
 
+func NewResponseWriter(rw http.ResponseWriter) ResponseWriter {
+	newRw := responseWriter{rw, 0, 0, nil}
+	if cn, ok := rw.(http.CloseNotifier); ok {
+		return &closeNotifyResponseWriter{newRw, cn}
+	}
+	return &newRw
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	status     int
+	size       int
+	BeforeFunc []BeforeFunc
+}
+
+type closeNotifyResponseWriter struct {
+	responseWriter
+	closeNotifier http.CloseNotifier
+}
